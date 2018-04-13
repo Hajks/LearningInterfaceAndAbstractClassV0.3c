f4f41ef137d4b69932c93fabc27fc6aaa233e551
Hajk <tunczorx@gmail.com>
2018-04-13 10:06:37 +0200

    Using interitance with Interfaces.

---
+++ LearningInterfaceAndAbstractClassV0.3c/IStingPatrol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningInterfaceAndAbstractClassV0._3c
+{
+    interface IStingPatrol : IWorker // So now if we decide to implement IStingPatrol, we have to include in our class all elements from this interface and IWorker interface 
+    {
+        int AlertLevel { get; }
+        int StingerLength { get; set; }
+        bool LookForEnemies();
+        int SharpenStinger(int Length);
+    }
+}
---
+++ LearningInterfaceAndAbstractClassV0.3c/IWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningInterfaceAndAbstractClassV0._3c
+{
+    interface IWorker
+    {
+        string job{get; }
+        int ShiftsLeft { get; }
+        void DoThisJob(string job, int shifts);
+        void WorkOneShift();
+    }
+}
